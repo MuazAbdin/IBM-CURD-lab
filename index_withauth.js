32839aa74871459f6102e61f2eedf1f6eb95bd49
Muaz Abdeen <muaz.abdeen@cs.huji.ac.il>
2023-06-17 20:44:25 +0300

    final ..!

--- index_withauth.js
+++ index_withauth.js
@@ -4,47 +4,60 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 
 const app = express();
-const PORT =5000;
+const PORT = 5000;
 
+/* This tells your express app to use the session middleware.
+ *
+ *   secret - a random unique string key used to authenticate a session.
+ *   resave - takes a Boolean value. It enables the session to be stored 
+ *            back to the session store, even if the session was never 
+ *            modified during the request.
+ *   saveUninitialized - this allows any uninitialized session to be sent 
+ *                       to the store. When a session is created but not 
+ *                       modified, it is referred to as uninitialized.
+ * 
+ * The default value of both resave and saveUninitialized is true, but the 
+ * default is deprecated. So, set the appropriate value according to the use case. */
 app.use(session({secret:"fingerpint",resave: true, saveUninitialized: true}))
 
 app.use(express.json());
 
-app.use("/user", (req,res,next)=>{
-// Middleware which tells that the user is authenticated or not
-   
+/* Observe the implementation of the authentication middleware. 
+ * All the endpoints starting with /user will go through this middleware.
+ * It will retrieve the authorization details from the session and verify it. 
+ * If the token is validated, the user is authenticated and the control is 
+ * passed on to the next endpoint handler. If the token is invalid, the user 
+ * is not authenticated and an error message is returned. */
+app.use("/user", (req, res, next) => {
    if(req.session.authorization) {
        let token = req.session.authorization['accessToken']; // Access Token
        
        jwt.verify(token, "access",(err,user)=>{
-           if(!err){
+           if (!err) {
                req.user = user;
                next();
            }
-           else{
-               return res.status(403).json({message: "User not authenticated"})
-           }
+           else { return res.status(403).json({message: "User not authenticated"}); }
         });
-    } else {
-        return res.status(403).json({message: "User not logged in"})
-    }
+    } else { return res.status(403).json({message: "User not logged in"}); }
 });
 
 app.use("/user", routes);
 
+/* Observe the implementation of the login endpoint.
+ * A user logs into the system providing a username. An access token that 
+ * is valid for one hour is generated. You may observe this validty length 
+ * specified by 60 * 60, which signifies the time in seconds. This access 
+ * token is set into the session object to ensure that only authenticated 
+ * users can access the endpoints for that length of time. */
 app.post("/login", (req,res) => {
     const user = req.body.user;
-    if (!user) {
-        return res.status(404).json({message: "Body Empty"});
-    }
-    let accessToken = jwt.sign({
-        data: user
-      }, 'access', { expiresIn: 60 * 60 });
-
-      req.session.authorization = {
-        accessToken
-    }
+    if (!user) { return res.status(404).json({message: "Body Empty"}); }
+    let accessToken = jwt.sign(
+        { data: user }, 'access', { expiresIn: 60 * 60 }
+    );
+    req.session.authorization = { accessToken };
     return res.status(200).send("User successfully logged in");
 });
 
-app.listen(PORT,()=>console.log("Server is running at port "+PORT));
+app.listen(PORT, () => console.log("Server is running at port " + PORT) );
--- routes/users.js
+++ routes/users.js
@@ -23,37 +23,72 @@ let users = [
     },
 ];
 
+const getDateFromString = (strDate) => {
+  let [dd,mm,yyyy] = strDate.split('-');
+  return new Date(yyyy+"/"+mm+"/"+dd);
+}
+
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  res.send(JSON.stringify({users},null,4));
+});
+
+
+router.get('/sort', (req, res) => {
+  users.sort( (usr1, usr2) => getDateFromString(usr1.DOB) - getDateFromString(usr2.DOB) );
+  res.send(JSON.stringify({users},null,4));
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  res.send(users.filter( user => user.email===req.params.email ));
+});
+
+// curl localhost:5000/user/firstName/John
+router.get("/firstName/:firstName", (req, res) => {
+  res.send( users.filter( user => user.firstName === req.params.firstName) );
 });
 
 
 // POST request: Create a new user
 router.post("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  users.push(
+    {
+      "firstName": req.query.firstName,
+      "lastName": req.query.lastName,
+      "email": req.query.email,
+      "DOB": req.query.DOB
+    }
+  );
+  res.send(`${req.query.firstName} ${req.query.lastName} has been added to users.`);
 });
 
 
 // PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  let filteredUsers = users.filter( user => user.email === email);
+  if (filteredUsers.length > 0) {
+    let filteredUser = filteredUsers[0];
+    for (key in req.query) {
+      attribute = req.query[key];
+      if (attribute) { filteredUser[key] = attribute; }
+    }
+    users = users.filter((user) => user.email != email);
+    users.push(filteredUser);
+    res.send(`User with the email ${email} updated.`);
+  } else {
+    res.send("Unable to find user!");
+  }
 });
 
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  users = users.filter( user => user.email != req.params.email );
+  res.send(`User with email: ${req.params.email} is deleted, 
+            ${users.length} users are remaining.
+            Users: \n ${JSON.stringify({users},null,4)}`);
 });
 
 module.exports=router;
